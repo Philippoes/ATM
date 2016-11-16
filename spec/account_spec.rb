9ae29ffd464c44d28650017c418cac27c5b54e0b
Philip <Philip.zdb@gmail.com>
2016-11-16 16:31:23 +0100

    added a spec test for raising error if no owner is set

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,8 +2,8 @@ require './lib/account.rb'
 
 describe Account do
   let(:person) { instance_double('Person', name: 'Philip') }
-  subject { described_class.new({owner: person})}
-  
+  subject { described_class.new({owner: person}) }
+
   it 'should have a 4 digit pin_code' do
     pin_code_length = Math.log10(subject.pin_code).to_i + 1
     expect(pin_code_length).to eq 4
@@ -23,4 +23,12 @@ describe Account do
     expect(subject.account_status).to eq :deactivated
   end
   #because deactivating account using class method would deactivate all future accounts?
+
+  it 'is expected to have an owner' do
+    expect(subject.owner).to eq person
+  end
+
+  it 'is expected to raise error if no owner is set' do
+    expect { described_class.new }.to raise_error 'An account owner is required'
+  end
 end
