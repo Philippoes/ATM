56fc4d2c089edb90f47ba92fef48e664134a258a
Philip <Philip.zdb@gmail.com>
2016-11-16 12:11:33 +0100

    Added exp date for 5 years, refactored pin code

--- lib/account.rb
+++ lib/account.rb
@@ -2,10 +2,22 @@ class Account
   attr_accessor :funds, :account_status
   attr_reader :pin_code, :exp_date
 
+  STANDARD_VALIDITY_YRS = 5
+  
   def initialize
     @funds = 1000
-    @pin_code = rand(1000..9999)
-    @exp_date = '12/18'
+    @pin_code = set_pin_code
+    @exp_date = set_exp_date
     @account_status = :Active
+  end
+
+  private
+
+  def set_pin_code
+    rand(1000..9999)
+  end
+
+  def set_exp_date
+    Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
   end
 end
