1b3c13c8fa397be7954dd4af44fc2577a18061c4
Philip <Philip.zdb@gmail.com>
2016-11-16 16:48:14 +0100

    Fixed a bug so now all test works fine

--- lib/account.rb
+++ lib/account.rb
@@ -1,16 +1,17 @@
 require 'date'
 
 class Account
-  attr_accessor :funds, :account_status
+  attr_accessor :funds, :account_status, :owner
   attr_reader :pin_code, :exp_date
 
   STANDARD_VALIDITY_YRS = 5
 
-  def initialize
+  def initialize(attrs = {})
     @funds = 1000
     @pin_code = set_pin_code
     @exp_date = set_exp_date
     @account_status = :active
+    set_owner(attrs[:owner])
   end
 
   def set_pin_code
@@ -23,5 +24,15 @@ class Account
 
   def deactivate
     @account_status = :deactivated
+  end
+
+  private
+
+  def set_owner(obj)
+    obj == nil ? missing_owner : @owner = obj
+  end
+
+  def missing_owner
+    raise 'An account owner is required'
   end
 end
