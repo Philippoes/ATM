7a8b7f8e7a67a252af4b16d727f537c8442d731b
Philip <Philip.zdb@gmail.com>
2016-11-18 10:14:41 +0100

    refactored the person_spec file

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,7 +3,6 @@ require './lib/atm.rb'
 require './lib/account.rb'
 
 describe Person do
-  let(:atm) { Atm.new }
 subject { described_class.new(name: 'Philip', cash: 200) }
 
   it 'is expected to have :name on initialize' do
@@ -22,32 +21,41 @@ subject { described_class.new(name: 'Philip', cash: 200) }
     expect(subject.account).to be nil
   end
 
-  it 'can create an Account of Account Class and be the owner of that account' do
-      subject.create_account
+  describe 'can create an Account' do
+    before { subject.create_account }
+    it 'of account class' do
       expect(subject.account).to be_an_instance_of Account
+    end
+
+    it 'and be the owner of that account' do
       expect(subject.account.owner).to be subject
+    end
   end
 
-  it 'funds are added to the account upon deposit' do
-    subject.create_account
-    expected_output = { status: true, message: 'successful deposit', date: Date.today, amount: 100 }
-    expect(subject.deposit(100, subject.account.pin_code, subject.account, atm)).to eq expected_output
-  end
+  describe 'can manage funds if account has been created' do
+    let(:atm) { Atm.new }
+    before { subject.create_account }
 
-  it 'rejects deposit if pin is incorrect' do
+    it 'can not manage funds if no account has been created' do
+      account = nil
+      expect{subject.deposit(100, 9999, account, atm)}.to raise_error(RuntimeError, 'No account present')
+    end
+
+    it 'funds are added to the account upon deposit' do
+      expected_output = { status: true, message: 'successful deposit', date: Date.today, amount: 100 }
+      expect(subject.deposit(100, subject.account.pin_code, subject.account, atm)).to eq expected_output
+    end
+
+    it 'rejects deposit if pin is incorrect' do
     subject.create_account
     expected_output = { status: false, message: 'wrong pin', date: Date.today }
     expect(subject.deposit(100, 9999, subject.account, atm)).to eq expected_output
   end
 
-  it 'can not manage funds if no account has been created' do
-    account = nil
-    expect{subject.deposit(100, 9999, account, atm)}.to raise_error(RuntimeError, 'No account present')
-  end
-
   it 'receives cash upon atm wtihdrawal' do
     subject.create_account
-    command = lambda {subject.withdraw(100, subject.account.pin_code, subject.account, atm) }
+    command = lambda {subject.withdraw(100, subject.account.pin_code, subject.account, atm)}
     expect(command.call).to be_truthy
   end
+end
 end
